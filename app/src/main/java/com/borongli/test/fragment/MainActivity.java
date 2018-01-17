f19f504657b2e9d3b4647896eb64dff2a7eba414
Kirabusy <stalkerlee@163.com>
2018-01-17 15:48:51 +0800

    Initial commit

---
+++ app/src/main/java/com/borongli/test/fragment/MainActivity.java
@@ -0,0 +1,31 @@
+package com.borongli.test.fragment;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //test
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(getTaskId()));
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) return;
+            HeadlinesFragment firstFragment = new HeadlinesFragment();
+            firstFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+        }
+    }
+}
